6012ca9748f2e8f4bc6391a16ef7f0a7f0b017cb
Nick Doiron <ndoiron@mapmeld.com>
2011-03-03 21:37:48 -0500

    Posted plugin and bookmarklet

---
+++ config/population-estimator.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+* POPULATION ESTIMATOR CONFIGURATION
+*/
+
+/**
+ * Assuming use of ESRI's example World Population server
+ */
+
+$config['service_url'] = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/GPServer/PopulationSummary";
